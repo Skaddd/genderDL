556f01bb7a18f799aa462a22d71e442e05abe38c
Skaddd <60611609+Skaddd@users.noreply.github.com>
2020-08-27 07:59:15 +0200

    Update Window4.py

--- Window4.py
+++ Window4.py
@@ -3,21 +3,17 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QImage,QPixmap 
+from PyQt5.QtGui import QImage,QPixmap,QMovie,QColor
 from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtCore import QSize
 import source
 import glob
 import os
 import cv2
 import time
-
-from transfer_im import crop_center,load_image,styleT # functions that apply the style transfer model to a given image
-
-from detection.face_alignment import date
-
-
-
-
+from Window5 import Ui_Window5
+from transfer_im import crop_center,load_image,styleT 
+# functions that apply the style transfer model to a given image
 
 
 
@@ -26,11 +22,29 @@ class Ui_Window4(QMainWindow):
     def __init__(self):
         super(Ui_Window4,self).__init__()
 
+
+
+
+    def openWindow5(self):
+        self.qtim.stop()
+        self.ui5=Ui_Window5()
+        self.ui5.setupUi(self.language)
+        self.ui5.show()
+        self.ui5.stayBut.clicked.connect(lambda : self.ui5.closeWindow())
+        reset_timer = 300000
+        self.ui5.stayBut.clicked.connect(lambda : self.qtim.start(reset_timer))
+        self.ui5.leaveBut.clicked.connect(lambda : self.ui5.closeWindow())
+        self.ui5.leaveBut.clicked.connect(lambda : self.close())
+        self.ui5.windowTitleChanged.connect(lambda :self.close())
+
+
+
+    #we used alot of concepts already explained,
+    #we added a namestyle variable to know when a style is selected or not
+    #this method contains a huge part of object positioning (frames, layout ,...)
     def setupUi(self,lang):
         self.setObjectName("Window4")
         self.resize(1920, 1080)
-        #Window4.setStyleSheet("background-image: url(:/images/assets/back1.jpg);\n"
-        #"")
         self.setAnimated(True)
         self.language = lang
         self.centralwidget = QtWidgets.QWidget(self)
@@ -45,16 +59,39 @@ class Ui_Window4(QMainWindow):
                 "font: 14pt \"Adobe Pi Std\";\n"
                 "color: rgb(255, 255, 255);}\n"
             "QPushButton:pressed { border-style : inset; border-color:black}"
+            "QScrollBar:horizontal{"
+                "height: 20px;"
+                "margin: 3px 15px 3px 15px;"
+                "border-radius: 4px;"
+                "border: 1px transparent;"
+                "background-color: black;}"
+
+            "QScrollBar::handle:horizontal{"
+                "background-color: gray;"
+                "min-width: 5px;"
+                "border-radius: 4px;}"
+            "QScrollBar::add-line:horizontal{"
+                "margin : 0px 3px 0px 3px;"
+                "border-image: url(:/qss_icons/rc/right_arrow_disabled.png);"
+                "width: 10px;"
+                "height: 10px;}"
+            "QScrollBar::sub-line:horizontal{"
+                    "border-image: url(:/qss_icons/rc/left_arrow.png);"
+                    "height: 10px;"
+                    "width: 10px;}"
+
+            "QScrollBar::up-arrow:horizontal,QScrollBar::down-arrow:horizontal {"
+                "background : none;}"
+            "QScrollBar::add-page:horizontal, QScrollBar::sub-page:horizontal{"
+                "background: none;}"
             "#centralwidget{background-image: url(:/images/assets/back1.jpg);}")
 
-        #same as before since we are using once again camera and displaying content
         self.logic =1
         self.value =0
         self.stoploop=0
-        self.timer = 120000
-        #usefull, check below
+        self.timer = 600000
+        
         self.namestyle=""
-        #Title
         self.labeltitle = QtWidgets.QLabel(self.centralwidget)
         self.labeltitle.setGeometry(QtCore.QRect(50, 30, 1121, 61))
         self.labeltitle.setObjectName("titlelabel")
@@ -65,9 +102,11 @@ class Ui_Window4(QMainWindow):
         self.labeltitle.setScaledContents(True)
         self.labeltitle.setAlignment(QtCore.Qt.AlignCenter)
         self.labeltitle.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+
+
         #Box containing the camera output and images
         self.imgLabel = QtWidgets.QLabel(self.centralwidget)
-        self.imgLabel.setGeometry(QtCore.QRect(1200, 100, 512, 512))
+        self.imgLabel.setGeometry(QtCore.QRect(1200, 100, 512, 450))
         self.imgLabel.setFrameShape(QtWidgets.QFrame.Box)
         self.imgLabel.setFrameShadow(QtWidgets.QFrame.Raised)
         self.imgLabel.setAttribute(QtCore.Qt.WA_TranslucentBackground)
@@ -78,33 +117,77 @@ class Ui_Window4(QMainWindow):
 
 
         #Frame to put buttons
-        self.frameButtons=QtWidgets.QFrame(self.centralwidget)
-        self.frameButtons.setAttribute(QtCore.Qt.WA_TranslucentBackground)
-        self.frameButtons.setFrameShape(QtWidgets.QFrame.StyledPanel)
-        self.frameButtons.setFrameShadow(QtWidgets.QFrame.Raised)
-        self.frameButtons.setObjectName("frameButtons")
-        self.frameButtons.setGeometry(QtCore.QRect(1100,600,750,91))
+        self.frameLeft=QtWidgets.QFrame(self.centralwidget)
+        self.frameLeft.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        self.frameLeft.setObjectName("frameLeft")
+        self.frameLeft.setGeometry(QtCore.QRect(1100,600,800,250))
+        
+        self.gridframe=QtWidgets.QGridLayout(self.frameLeft)
+        self.gridframe.setObjectName("gridframe")
 
-        self.captureBut = QtWidgets.QPushButton(self.frameButtons)
-        self.captureBut.setGeometry(QtCore.QRect(50, 30, 141, 41))
+        self.VBoxtop = QtWidgets.QVBoxLayout()
+        self.VBoxtop.setObjectName("VBoxtop")
+        self.VBoxbot=QtWidgets.QVBoxLayout()
+        self.VBoxbot.setObjectName("VBoxbot")
+        
+
+        self.HBoxtop = QtWidgets.QHBoxLayout()
+        self.HBoxtop.setObjectName("HBoxtop")
+        self.HBoxtop.setSpacing(110)
+        
+
+
+        self.captureBut = QtWidgets.QPushButton(self.frameLeft)
+        self.captureBut.setFixedSize(QSize(141,41))
         self.captureBut.setMouseTracking(True)
         self.captureBut.setObjectName("captureBut")
 
-        self.screenBut = QtWidgets.QPushButton(self.frameButtons)
-        self.screenBut.setGeometry(QtCore.QRect(300, 30, 141, 41))
+        self.screenBut = QtWidgets.QPushButton(self.frameLeft)
+        self.screenBut.setFixedSize(QSize(141,41))
         self.screenBut.setMouseTracking(True)
         self.screenBut.setObjectName("screenBut")
 
 
-        self.transferBut = QtWidgets.QPushButton(self.frameButtons)
-        self.transferBut.setGeometry(QtCore.QRect(550, 30, 141, 41))
+        self.transferBut = QtWidgets.QPushButton(self.frameLeft)
+        self.transferBut.setFixedSize(QSize(141,41))
         self.transferBut.setMouseTracking(True)
         self.transferBut.setObjectName("transferBut")
+
+        self.waitingLabel = QtWidgets.QLabel(self.frameLeft)
+        self.waitingLabel.setAlignment(QtCore.Qt.AlignLeft| QtCore.Qt.AlignVCenter)
+        self.gif=QMovie('assets/load6.gif')
+        self.gif.setScaledSize(QSize(400,300))
+        self.gif.setBackgroundColor(QColor("transparent"))
+        self.waitingLabel.setMovie(self.gif)
+        
+
+        self.waitingLabel2=QtWidgets.QLabel(self.frameLeft)
+        self.waitingLabel2.setObjectName("waitingLabel2")
+        self.waitingLabel2.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        self.waitingLabel2.setStyleSheet(
+            "font: italic  18pt \"Adobe Pi Std\";\n"
+            "color: rgb(255, 255, 255);")
+        self.waitingLabel2.hide()
+        
+
+        self.HBoxtop.addWidget(self.captureBut)
+        self.HBoxtop.addWidget(self.screenBut)
+        self.HBoxtop.addWidget(self.transferBut)
+
+        self.VBoxtop.addLayout(self.HBoxtop)
+        self.gridframe.addLayout(self.VBoxtop,0,0,1,1)
+
+        self.VBoxbot.addWidget(self.waitingLabel, alignment=QtCore.Qt.AlignVCenter |QtCore.Qt.AlignHCenter )
+        self.VBoxbot.addWidget(self.waitingLabel2, alignment=QtCore.Qt.AlignVCenter |QtCore.Qt.AlignHCenter )
+    
+        self.gridframe.addLayout(self.VBoxbot,1,0,1,1)
+
+
 
         self.infoLabel = QtWidgets.QLabel(self.centralwidget)
         self.infoLabel.setGeometry(QtCore.QRect(30, 680, 1100, 300))
         self.infoLabel.setStyleSheet(
-            "font: italic 14pt \"Adobe Pi Std\";\n"
+            "font: italic 16pt \"Adobe Pi Std\";\n"
             "color: rgb(255, 255, 255);")
         
         self.infoLabel.setObjectName("infoLabel")
@@ -211,13 +294,13 @@ class Ui_Window4(QMainWindow):
             self.screenBut.setText("Prendre photo")
             self.transferBut.setText("Application  style")
             self.infoLabel.setText(
-                "Applique different style à ton image!\n"
-                " C'est très simple, avant tout si tu n'as pas encore pris de photo de toi  lance la camera et prend une photo quand tu es prêt!\n"
-                "Il y a un décalage de 5 secondes.\n"
-                "Si jamais tu veux en prendre une autre tu peux!\n"
-                "Maintenant que tu as une belle image,Choisi le style que tu aimes dans le catalogue d'image à gauche!\n"
-                "Une fois que tu as choisi le style que tu veux en appuyant sur le bouton correspondant, il ne te reste plus qu'à appliquer le style à ton image!\n"
-                "Cela peut prendre un peu de temps, essaye de ne pas appuyer sur tout les boutons en attendant...")
+                "1. Applique differents styles à ton image!\n"
+                "2. Si tu n'as pas encore pris de photo de toi lance la camera et prend une photo quand tu es prêt!\n"
+                "3. La Photo sera prise après 5 secondes.\n"
+                "4. Choisi le style que tu aimes dans le catalogue d'image au dessus!\n"
+                "5. Une fois que tu as choisi le style que tu veux en appuyant sur le bouton correspondant, applique le style\n"
+                "6. Soit patient cela prend du temps\n")
+
 
 
         elif self.language==1:
@@ -227,18 +310,38 @@ class Ui_Window4(QMainWindow):
             self.screenBut.setText("Take picture")
             self.transferBut.setText("Apply Style")
             self.infoLabel.setText(
-                "Apply different style to your image!\n"
-                "It's simple, First if you haven't  taken yet an image of yourself launch the camera with the button caputre and take a picture!\n"
-                "It will take a photo within 5 seconds.\n"
-                "Feel free to take another one if you don't like the last one!\n"
-                "Now you have a beautfil image , Select the style you like from the different images presented!\n"
-                "Once you selected your style use the button  Apply Style to see the result!\n"
-                "The process might take some time, be Kind don't spam the other buttons ")
+                "1. Apply different style to your image!\n"
+                "2. First if you haven't taken yet an image of yourself launch the camera with the button caputre and take a picture!\n"
+                "3. It will take a photo within 5 seconds.\n"
+                "4. Select a style image from the different images presented above!\n"
+                "5. Once you selected your style use the button  Apply Style to see the result!\n"
+                "6. Be patient, it might take some time\n")
 
         elif self.language ==2:
-            pass
+            self.labeltitle.setText("")
+            self.backBut.setText("")
+            self.captureBut.setText("")
+            self.screenBut.setText("")
+            self.transferBut.setText("")
+            self.infoLabel.setText("")
+
+        elif self.language==3:
+            self.labeltitle.setText("")
+            self.backBut.setText("")
+            self.captureBut.setText("")
+            self.screenBut.setText("")
+            self.transferBut.setText("")
+            self.infoLabel.setText("")
+
         else:
-            pass
+            self.labeltitle.setText("")
+            self.backBut.setText("")
+            self.captureBut.setText("")
+            self.screenBut.setText("")
+            self.transferBut.setText("")
+            self.infoLabel.setText("")
+
+
         
         self.captureBut.clicked.connect(lambda: self.mousePressEvent(QtGui.QMouseEvent.MouseButtonPress))
         self.screenBut.clicked.connect(lambda: self.mousePressEvent(QtGui.QMouseEvent.MouseButtonPress))
@@ -246,7 +349,7 @@ class Ui_Window4(QMainWindow):
                
         self.qtim = QtCore.QTimer()
         self.qtim.start(self.timer)
-        self.qtim.timeout.connect(self.close)
+        self.qtim.timeout.connect(self.openWindow5)
         
         self.setCentralWidget(self.centralwidget)
         self.retranslateUi()
@@ -255,8 +358,9 @@ class Ui_Window4(QMainWindow):
 
     def addtimer(self):
         print(self.qtim.remainingTime())
-        if self.qtim.remainingTime() <= 60000:
-            self.qtim.setInterval(self.timer)
+        if self.qtim.remainingTime() <= self.timer:
+            new_timer =  450000
+            self.qtim.setInterval(new_timer)
             self.qtim.start()
     
 
@@ -276,16 +380,33 @@ class Ui_Window4(QMainWindow):
         return lambda : self.storeButname(k)
 
     def storeButname(self,k):
+        self.waitingLabel2.show()
+        if self.language==0:
+            self.waitingLabel2.setText("Appliquer le style puis patientez...")
+        elif self.language==1:
+            self.waitingLabel2.setText("Apply the style then wait...")
+        elif self.language ==2:
+            self.waitingLabel2.setText("")
+        elif  self.language==3:
+            self.waitingLabel2.setText("")
+        else :
+            self.waitingLabel2.setText("")
+
         self.namestyle = self.buttons[(k)].text()
 
     ##
 
     def leavingWin(self):
+        self.imgLabel.clear()
         self.stoploop=1
         self.close()
 
     def launchCamera(self,TIMER):
+        self.waitingLabel2.hide()
+        self.gif.start()
         self.logic=1
+        self.stoploop=0
+        font = cv2.FONT_HERSHEY_SIMPLEX
         cam=cv2.VideoCapture(0)
         self.imgLabel.clear()
         while (cam.isOpened()  and self.stoploop==0) :
@@ -300,6 +421,13 @@ class Ui_Window4(QMainWindow):
 
                 while TIMER >= 0: 
                     ret, img = cam.read()
+                    if TIMER== 0:
+                        pass
+                    else: 
+                        cv2.putText(img, str(TIMER),  
+                            (100, 125), font, 
+                            2, (105, 105, 105), 
+                            4, cv2.LINE_AA)
                     self.displayImage(img)
                     cv2.waitKey() 
                     cur = time.time() 
@@ -310,16 +438,30 @@ class Ui_Window4(QMainWindow):
                 else :
                     date=time.strftime("%Y-%m-%d-%H-%M")
                     cv2.imwrite('portrait/im-'+date+'.jpg',img)
-                    cv2.waitKey(125)
-                    self.imgLabel.clear()
+                    cv2.waitKey(500)
+                    self.enableBut()
                     self.value+=1
                     break
 
                                             
         cam.release()
         cv2.destroyAllWindows()
+    
+
+    def disableBut(self):
+        self.screenBut.setEnabled(False)
+        self.captureBut.setEnabled(False)
+        self.transferBut.setEnabled(False)
+        self.backBut.setEnabled(False)
+
+    def enableBut(self):
+        self.screenBut.setEnabled(True)
+        self.captureBut.setEnabled(True)
+        self.transferBut.setEnabled(True)
+        self.backBut.setEnabled(True)
 
     def takePicture(self):
+        self.disableBut()
         self.logic+=1
         
 
@@ -339,16 +481,40 @@ class Ui_Window4(QMainWindow):
     #pretty much the same as displayResults, we get the last image, 
     #we check that a style image was chosen thanks to the variable namestyle that will store the last button's name pressed(nameBut +jpg = path)
     def displayTransfer(self):
+        self.waitingLabel2.show()
         path = os.path.join("portrait","*")
         list_images = glob.glob(path)
-        latest_img = max(list_images,key=os.path.getctime)
-        if self.namestyle=="":
+        
+        if self.namestyle=="" or  not list_images:
             print("you did not selected an image")
+            if self.language==0:
+                self.waitingLabel2.setText("Aucune image de style selectionnée / Aucune image prises")
+            elif self.language==1:
+                self.waitingLabel2.setText("No style image were selected / No image taken")
+            elif self.langugage ==2:
+                self.waitingLabel2.setText("")
+            elif self.language==3:
+                self.waitingLabel2.setText("")
+            else:
+                self.waitingLabel2.setText("")
         else :
-            styleT(latest_img,'pictures/'+self.namestyle+'.jpg')
+            latest_img = max(list_images,key=os.path.getctime)
+            self.stoploop=1
+                    
+            path_img = 'pictures/'+self.namestyle
+            if os.path.isfile(path_img+'.png'):
+                path_img = path_img+'.png'
+            elif os.path.isfile(path_img+'.jpg'):
+                path_img = path_img+'.jpg'
+            elif os.path.isfile(path+'.jpeg'):
+                path_img = path_img+'.jpeg'
+            else :
+                print("Le type de l'image n'est pas supportée")
+                return 
+            styleT(latest_img,path_img)
             im_transfer = cv2.imread('transfer.jpg')
             self.displayImage(im_transfer)
-
+            self.waitingLabel2.hide()
 
 
 
@@ -358,10 +524,11 @@ class Ui_Window4(QMainWindow):
         self.setWindowTitle(_translate("Window4", "MainWindow"))
  
 
-
-
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
+    ui=Ui_Window4()
+    ui.setupUi(lang=1)
+    ui.show()
     Window4 = QtWidgets.QMainWindow()
     sys.exit(app.exec_())
