5c4b7221ddda1c392bcfff8e27724d05e4313cee
Skaddd <60611609+Skaddd@users.noreply.github.com>
2020-08-28 15:42:54 +0200

    Update face_alignment.py

--- detection/face_alignment.py
+++ detection/face_alignment.py
@@ -2,18 +2,21 @@ import sys
 import dlib
 import cv2
 import time
-predictor_path = './detection/shape_predictor_68_face_landmarks.dat'
+import os
+predictor_path = 'detection\\shape_predictor_68_face_landmarks.dat'
 #face_file_path = './classe.jpg'
-date=time.strftime("%Y-%m-%d-%H-%M")
+
 def face_ali_detec(face_file_path):
+    date=time.strftime("%Y-%m-%d-%H-%M")
     print(face_file_path)
     #Loading models we need , predictor for landmarks used for face alignment
     detector = dlib.get_frontal_face_detector()
-    sp = dlib.shape_predictor(predictor_path)
+    sp = dlib.shape_predictor(resource_path(predictor_path))
 
 
     img = cv2.imread(face_file_path)
     img_raw=img.copy()
+    print(img_raw.shape)
     # Ask the detector to find the bounding boxes of each face. The 1 in the
     # second argument indicates that we should upsample the image 1 time. This
     # will make everything bigger and allow us to detect more faces.
@@ -22,7 +25,7 @@ def face_ali_detec(face_file_path):
     num_faces = len(dets)
     if num_faces == 0:
         print("Sorry, there were no faces found in '{}'".format(face_file_path))
-        exit()
+        raise Exception
 
     # Find the 5 face landmarks we need to do the alignment.
     faces = dlib.full_object_detections()
@@ -37,16 +40,23 @@ def face_ali_detec(face_file_path):
     images = dlib.get_face_chips(img, faces, size=227)
 
     for i,image  in enumerate(images):
-        outfile_1= './detection/images/aligned/aligned- {} --'.format(i)+date+'.png'
-        print(outfile_1)
-        cv2.imwrite(outfile_1,image)
+        outfile_1= 'detection\\images\\aligned\\tmp\\aligned-{}--'.format(i)+date+'.png'
+        cv2.imwrite(resource_path(outfile_1),image)
     
 
     return rects
 
 
 
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
 
+    return os.path.join(base_path, relative_path)
 
 
 '''
